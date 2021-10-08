a3a0640f25dce152ff7112e4692e4ed9e5a58e53
Ayush <ayushsenapati123@gmail.com>
2021-10-08 11:32:25 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
 import "./App.css";
+//! Importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      {/* <h1>Hello React</h1>
       <p>
         Formula of Hydrogen is :- H<sub>2</sub>O
-      </p>
+      </p> */}
+      <header>
+        <h1>Ayush's To-Do List</h1>
+      </header>
+
+      <Form />
+      <TodoList />
     </div>
   );
 }
