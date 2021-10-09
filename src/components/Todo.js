1cf9d131a1f937477e8732afae9dce390da29411
Ayush <ayushsenapati123@gmail.com>
2021-10-09 11:21:50 +0530

    Initialized completeHandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,16 +4,30 @@ const Todo = ({ text, todo, todos, setTodos }) => {
   //! Events
   const deleteHandler = () => {
     // console.log(todo);
-    console.log(todo.id)
-    setTodos(todos.filter((el) => console.log(el.id)));
+    console.log(todo.id);
+    setTodos(todos.filter((xyzelement) => console.log(xyzelement.id)));
     //! Use Inspect element and see the values to understand
 
     // filter function discards the element from the array if the condition is not satisfied so here the condition is that the id should not be the same , but since the id's are same , the condition fails(false) so it filters/removes it from the array.
     // So array se hatega voh jiska condition match nahi karta.
     // The filter() method creates an array filled with all array elements that pass a test (provided by a function).
-    setTodos(todos.filter((el) => el.id !== todo.id));
-    
+    setTodos(todos.filter((xyzelement) => xyzelement.id !== todo.id));
   };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((xyzitem) => {
+        if (xyzitem.id === todo.id) {
+          return {
+            ...xyzitem,
+            completed: !xyzitem.completed,
+          };
+        }
+        return xyzitem;
+      })
+    );
+  };
+
   return (
     <div className="todo">
       {/* <li className="todo-item">HEY GUYS</li> */}
